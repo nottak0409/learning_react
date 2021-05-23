e96826e10fc2230c8b03b9e6816fa66d883e4837
nottak0409 <nottak0409@gmail.com>
2021-05-23 15:02:26 +0900

    class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            testEdit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//            testEdit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//       >
+//      Learn React
+//        </a>
+//      </header>
+//    </div>
+// );
+//}
+const App = () => {
+    return (
+        <div>
+            aaaa
+            <cat/>
+            <cat/>
+            <cat/>
+        </div>
+    )
 }
 
 export default App;
